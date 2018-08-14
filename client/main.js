5bdacb2398a0faa1b9a5702bbe69b32b2597eceb
Matt Bell <mappum@gmail.com>
2018-08-14 15:55:15 -0700

    Extracted markup into component functions

--- client/main.js
+++ client/main.js
@@ -11,41 +11,80 @@ app.mount('body')
 function mainView (state, emit) {
   return html`
     <body>
-      <header>
-        <div>
-          <h1>cryptodepth</h1>
-        </div>
-      </header>
+      ${header()}
       <section class="main">
-        <table>
-          <tr>
-            <th>#</th>
-            <th>Asset</th>
-            <th>Effective Value</th>
-            <th>Market Share</th>
-            <th>Cost to 2X Price</th>
-            <th>1% Dump</th>
-            <th>
-              <a class="suggest" href="https://github.com/mappum/cryptodepth.co/issues/new?labels=indicator%20suggestion">
-                Suggest An Indicator
-              </a>
-            </th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>BTC</td>
-            <td>$168,000,000</td>
-            <td>60.3%</td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </table>
+        ${table(state.data)}
       </section>
     </body>
   `
 }
 
-function store (state, emitter) {
+function header () {
+  return html`
+    <header>
+      <div>
+        <h1>cryptodepth</h1>
+      </div>
+    </header>
+  `
+}
 
+function table (data) {
+  return html`
+    <table>
+      <tr>
+        <th>#</th>
+        <th>Asset</th>
+        <th>Effective Value</th>
+        <th>Market Share</th>
+        <th>Cost to 2X Price</th>
+        <th>1% Dump Price</th>
+        <th>
+          <a class="suggest" href="https://github.com/mappum/cryptodepth.co/issues/new?labels=indicator%20suggestion">
+            Suggest An<br>Indicator
+          </a>
+        </th>
+      </tr>
+      ${data.map(row)}
+    </table>
+  `
+}
+
+function row (data, i) {
+  return html`
+    <tr>
+      <td>${i + 1}</td>
+      <td><strong>${data.symbol}</strong></td>
+      <td>${formatDollars(data.effectiveValue)}</td>
+      <td>${formatPercent(data.marketShare)}</td>
+      <td>${formatDollars(data.costTo2x)}</td>
+      <td>${formatPercent(-data.dump1Percent)}</td>
+      <td></td>
+    </tr>
+  `
+}
+
+function formatDollars (n) {
+  return '$' + n.toLocaleString()
+}
+
+function formatPercent (n) {
+  // TODO: change precision for small numbers
+  return n.toFixed(1) + '%'
+}
+
+function store (state, emitter) {
+  // TODO: fetch data from API
+  state.data = [
+    {
+      symbol: 'BTC',
+      effectiveValue: 1234567890,
+      marketShare: 60.345,
+      costTo2x: 1234567890,
+      dump1Percent: 4.567
+    }
+  ]
+  for (let i = 0; i < 5; i++) {
+    state.data = state.data.concat(state.data)
+  }
 }
